8da8342fa6dfb3759956539dea184db4e01276a9
alex voytan <arvoytan@mail.csuchico.edu>
2020-05-12 14:31:15 -0700

    add import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:discount_alcohol/main.dart';
-
+import 'package:../discount_alcohol/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
