bfa314a7a32646dca8b6b30e238ebc568ec953b6
alex voytan <arvoytan@mail.csuchico.edu>
2020-05-13 14:35:02 -0700

    add main test

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:discount_alcohol/main.dart';
+import 'package:main.dart';
 
 void main() {
   testWidgets('Check for all alcohol buttons', (WidgetTester tester) async {
