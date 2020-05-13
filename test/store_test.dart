03aba3fde0673622e2e21221945c62cff877afc3
alex voytan <arvoytan@mail.csuchico.edu>
2020-05-13 13:40:11 -0700

    add store test

--- test/store_test.dart
+++ test/store_test.dart
@@ -1,4 +1,5 @@
-import 'package:test/test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:discount_alcohol/store.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
