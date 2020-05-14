fa099a605e1f34d3cc3143625bc30314eec68e73
alex voytan <arvoytan@mail.csuchico.edu>
2020-05-14 15:58:06 -0700

    moving directories

--- test/store_test.dart
+++ test/store_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:discount_alcohol/store.dart';
 import 'package:discount_alcohol/main.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:discount_alcohol/mapPage.dart';
 void main() {
   test('Check Store initialization', () {
     Store store = Store("Store A", LatLng(20.0, 30.0), 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5);
@@ -12,7 +12,7 @@ void main() {
     bool vodkaEquals = store.findAlcoholPrice("Vodka") == 4.5;
     bool whiskeyEquals = store.findAlcoholPrice("Whiskey") == 5.5;
     bool ginEquals = store.findAlcoholPrice("Gin") == 6.5;
-    bool champagneEquals = store.findAlcoholPrice("Champange") == 7.5;
+    bool champagneEquals = store.findAlcoholPrice("Champagne") == 7.5;
     bool rumEquals = store.findAlcoholPrice("Rum") == 8.5;
     bool tequilaEquals = store.findAlcoholPrice("Tequila") == 9.5;
     bool good = beerEquals && wineEquals && vodkaEquals && whiskeyEquals && ginEquals && champagneEquals && rumEquals && tequilaEquals;
@@ -25,6 +25,14 @@ void main() {
     expect(fakePrice, 100000);
   });
 
-
+ test('calculate distance check', (){
+   MapPageFrame store;
+   double distance = store.calculateDistance(10,20,30,40);
+   expect(distance, 1889.342998);
+ });
 }
+
+
+
+
 
