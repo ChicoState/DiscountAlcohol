320c5bbd75b400a51da9f4fa2062959cdb68e408
alex voytan <arvoytan@mail.csuchico.edu>
2020-05-14 12:52:13 -0700

    add map tests

--- test/map_test.dart
+++ test/map_test.dart
@@ -4,11 +4,76 @@ import 'package:discount_alcohol/mapPage.dart';
 
 void main() {
   testWidgets('Filter test: Beer', (WidgetTester tester) async {
-    /*await tester.pumpWidget(MapPage("Beer"));
+    await tester.pumpWidget(MapPage("Beer"));
     await tester.tap(find.byType(DropdownButtonFormField));
     await tester.pump();
     await tester.tap(find.text("10 mi"));
-    await tester.pump();*/
+    await tester.pump();
+
+
+  });
+
+  testWidgets('Filter test: Wine', (WidgetTester tester) async {
+    await tester.pumpWidget(MapPage("Wine"));
+    await tester.tap(find.byType(DropdownButtonFormField));
+    await tester.pump();
+    await tester.tap(find.text("1 mi"));
+    await tester.pump();
+
+
+  });
+
+  testWidgets('Filter test: Vodka', (WidgetTester tester) async {
+    await tester.pumpWidget(MapPage("Vodka"));
+    await tester.tap(find.byType(DropdownButtonFormField));
+    await tester.pump();
+    await tester.tap(find.text("3 mi"));
+    await tester.pump();
+
+
+  });
+  testWidgets('Filter test: Whiskey', (WidgetTester tester) async {
+    await tester.pumpWidget(MapPage("Whiskey"));
+    await tester.tap(find.byType(DropdownButtonFormField));
+    await tester.pump();
+    await tester.tap(find.text("5 mi"));
+    await tester.pump();
+
+
+  });
+  testWidgets('Filter test: Gin', (WidgetTester tester) async {
+    await tester.pumpWidget(MapPage("Gin"));
+    await tester.tap(find.byType(DropdownButtonFormField));
+    await tester.pump();
+    await tester.tap(find.text("20 mi"));
+    await tester.pump();
+
+
+  });
+  testWidgets('Filter test: Champange', (WidgetTester tester) async {
+    await tester.pumpWidget(MapPage("Champange"));
+    await tester.tap(find.byType(DropdownButtonFormField));
+    await tester.pump();
+    await tester.tap(find.text("50 mi"));
+    await tester.pump();
+
+
+  });
+  testWidgets('Filter test: Rum', (WidgetTester tester) async {
+    await tester.pumpWidget(MapPage("Rum"));
+    await tester.tap(find.byType(DropdownButtonFormField));
+    await tester.pump();
+    await tester.tap(find.text("3 mi"));
+    await tester.pump();
+
+
+  });
+  testWidgets('Filter test: Tequila', (WidgetTester tester) async {
+    await tester.pumpWidget(MapPage("Tequila"));
+    await tester.tap(find.byType(DropdownButtonFormField));
+    await tester.pump();
+    await tester.tap(find.text("10 mi"));
+    await tester.pump();
 
 
   });
