774de775d95c18405f21fb135a380e4dcd204822
alex voytan <arvoytan@mail.csuchico.edu>
2020-05-14 12:52:43 -0700

    add store tests

--- test/store_test.dart
+++ test/store_test.dart
@@ -10,18 +10,20 @@ void main() {
     bool wineEquals = store.findAlcoholPrice("Wine") == 3.5;
     bool vodkaEquals = store.findAlcoholPrice("Vodka") == 4.5;
     bool whiskeyEquals = store.findAlcoholPrice("Whiskey") == 5.5;
-    bool ginEquals = store.findAlcoholPrice("gin") == 6.5;
-    bool champagneEquals = store.findAlcoholPrice("champange") == 7.5;
-    bool rumEquals = store.findAlcoholPrice("rum") == 8.5;
-    bool tequilaEquals = store.findAlcoholPrice("tequila") == 9.5;
+    bool ginEquals = store.findAlcoholPrice("Gin") == 6.5;
+    bool champagneEquals = store.findAlcoholPrice("Champange") == 7.5;
+    bool rumEquals = store.findAlcoholPrice("Rum") == 8.5;
+    bool tequilaEquals = store.findAlcoholPrice("Tequila") == 9.5;
     bool good = beerEquals && wineEquals && vodkaEquals && whiskeyEquals && ginEquals && champagneEquals && rumEquals && tequilaEquals;
-    expect(good, false);
+    expect(good, true);
   });
 
   test('Check alcohol not in list', () {
-   /* Store store = Store("Store A", LatLng(20.0, 30.0), 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5);
+    Store store = Store("Store A", LatLng(20.0, 30.0), 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5);
     double fakePrice = store.findAlcoholPrice("White Claw");
-    expect(fakePrice, 100000);*/
+    expect(fakePrice, 100000);
   });
+
+
 }
 
